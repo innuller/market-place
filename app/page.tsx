c890c7618477b0642f767a2a94775a82a86d0361
Reni Christian <renisonreni@gmail.com>
2024-10-13 01:52:48 +0530

    edit main page

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default async function Index() {
   return (
     <>
       {/* <Hero /> */}
-      <main className="flex-1 flex flex-col gap-6 px-4 justify-center items-center">
+      <main className="flex-1 flex flex-col gap-6 px-4 justify-center items-center h-screen">
         {/* <h2 className="font-medium text-xl mb-4">Next steps</h2>
         {hasEnvVars ? <SignUpUserSteps /> : <ConnectSupabaseSteps />} */}
         <Link href={"/NewHome"} className="bg-gradient-to-br from-green-400 to-secondary text-black font-bold py-2 px-4 rounded">
